28b40b931661719708d9f0bb972772fe0b95f74f
Holocrons <remi.wickuler@epitech.eu>
2019-10-29 02:10:23 +0100

    cyka

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -13,15 +13,43 @@ public class MovePlayer : MonoBehaviour
     private bool jump = false;
     private bool inAir = false;
     private Rigidbody rb;
+    public int playerNb = 1;
+    public List<RuntimeAnimatorController> animList;
+    private Animator anim;
+    public GameObject GroundCheck;
+    private GroundRaycast gc;
+    public List<GameObject> followPlayer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        gc = GroundCheck.GetComponent<GroundRaycast>();
     }
 
     private void Update()
     {
-        InputCatcher();
+        inAir = !gc.HasHit();
+        Debug.Log(inAir);
+        if (playerNb == 1)
+            InputCatcher();
+        else if (playerNb == 2)
+            InputCatcher2();
+        if (x != 0 && inAir == false)
+            anim.runtimeAnimatorController = animList[2];
+        else if (inAir == false)
+            anim.runtimeAnimatorController = animList[0];
+        else
+            anim.runtimeAnimatorController = animList[1];
+        foreach (GameObject obj in followPlayer)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y, obj.transform.position.z);
+            obj.transform.position = newPos;
+        }
+        if (x == 1)
+            transform.eulerAngles = new Vector3(0, 90, 0);
+        else if (x == -1)
+            transform.eulerAngles = new Vector3(0, -90, 0);
     }
 
     private void FixedUpdate()
@@ -39,11 +67,27 @@ public class MovePlayer : MonoBehaviour
         }
     }
 
+    void InputCatcher2()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            x = 1;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            x = -1;
+        else
+            x = 0;
+        if (Input.GetKeyDown(KeyCode.O))
+            Instantiate(lightPrefabs, transform.position, new Quaternion(0, 0, 0, 0));
+        if (Input.GetKeyDown(KeyCode.P))
+            Instantiate(lightPrefabs2, new Vector3(transform.position.x, transform.position.y, -20), new Quaternion(0, 0, 0, 0)).GetComponent<MovingLightX>().dir(transform.eulerAngles.y);
+        if (Input.GetKeyDown(KeyCode.UpArrow) && inAir == false)
+            jump = true;
+    }
+
     void InputCatcher()
     {
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.D))
             x = 1;
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.Q))
             x = -1;
         else
             x = 0;
--- Assets/Scripts/MovingLightX.cs
+++ Assets/Scripts/MovingLightX.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class MovingLightX : MonoBehaviour
 {
     public float speed;
-    
+    public float direction = -1;
+
     void Start()
     {
-        Destroy(this.gameObject, 3f);
+        Destroy(this.gameObject, 4f);
     }
 
     void Update()
     {
-        transform.Translate(new Vector3(speed * Time.deltaTime, 0 , 0));
+        transform.Translate(new Vector3(direction * speed * Time.deltaTime, 0 , 0));
+    }
+
+    public void dir(float angle)
+    {
+        if (angle == 90)
+            direction = 1;
+        else
+            direction = -1;
     }
 }
