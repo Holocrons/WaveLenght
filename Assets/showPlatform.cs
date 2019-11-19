6ff21836fac39616af295ea31f5fb47d0a1c1808
Holocrons <remi.wickuler@epitech.eu>
2019-11-19 01:07:37 +0100

    the game

---
+++ Assets/showPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showPlatform : MonoBehaviour
+{
+    GameObject tmp = null;
+    bool isctive = false;
+
+    void Update()
+    {
+        if (tmp == null)
+        {
+            Debug.Log("courge");
+            isctive = false;
+        }
+        GetComponent<MeshRenderer>().enabled = isctive;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("INIT");
+        if (other.tag == "blue")
+        {
+            isctive = true;
+            tmp = other.gameObject;
+        }
+    }
+}
