329f6668ad1abde188c42e4d7e259bfd38f639f0
blaise <blaise.carnevillier@epitech.eu>
2019-12-02 14:56:04 +0100

    MenuPause + prefab  MainMenu

---
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clicked() {
+        launchscene(staticField.scenenbr);
+    }
+    public void launchscene(int scene) {
+        switch (scene)
+        {
+            case 1:
+                SceneManager.LoadScene("SampleScene");
+                break;
+            case 2:
+                SceneManager.LoadScene("test1");
+                break;
+            case 3:
+                SceneManager.LoadScene("test2");
+                break;
+            case 4:
+                SceneManager.LoadScene("test3");
+                break;
+            default:
+                break;
+        }
+    }
+}
