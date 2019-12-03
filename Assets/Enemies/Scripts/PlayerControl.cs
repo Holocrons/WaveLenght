9145d27ee82ce6390038a98fcbdb91234d65b40c
unknown <alexandre.gueguen@epitech.eu>
2019-12-03 09:50:23 +0900

    drone explosion with some light

--- Assets/Enemies/Scripts/PlayerControl.cs
+++ Assets/Enemies/Scripts/PlayerControl.cs
@@ -28,10 +28,10 @@ public class PlayerControl : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision)
     {
-        // if (collision.gameObject.CompareTag("Enemy")) {
-        //     EnemyStat stat = collision.gameObject.GetComponent<EnemyStat>();
-        //     if (stat)
-        //         stat.TakeDamage(1);
-        // }
+        if (collision.gameObject.CompareTag("Enemy")) {
+            EnemyStat stat = collision.gameObject.GetComponent<EnemyStat>();
+            if (stat)
+                stat.TakeDamage(1);
+        }
     }
 }
